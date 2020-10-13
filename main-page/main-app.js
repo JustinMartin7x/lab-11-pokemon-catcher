61b05676d107e707e30c7d5fa912834814cc4a67
JustinMartin7x <justin.martin7x@gmail.com>
2020-10-12 20:58:20 -0700

    stuck on how to fix for loop

--- main-page/main-app.js
+++ main-page/main-app.js
@@ -1,23 +1,46 @@
 //importing functions
-import { randomEncounter } from './utils.js';
+import { randomEncounter, findById } from './utils.js';
 import { pokemonList } from '../pokemon-data/poke-data.js';
 
 
 //Grabbing DOM elements
 const selection = document.querySelectorAll('input');
 const selectionImages = document.querySelectorAll('img');
-
+const catchButton = document.getElementById('button');
 
 
 //States
 let pokemonEncounters = 10;
-let pokemonCaught= [];
+let pokemonCaught = [];
 const pokemonSeen = [];
+let loopArray = [];
 
 //generate new pokemon
-for (let i = 0; i < selectionImages.length; i++) {
-    const newPokemon = randomEncounter(pokemonList);
-    console.log(newPokemon.url_image);
-    selectionImages[i].src = newPokemon.url_image;
-    selection[i].value = newPokemon.id;
+generatePokemon();
+
+catchButton.addEventListener('click', () => {
+    pokemonEncounters--;   
+    for (let i = 0; i < selection.length; i++) {
+        pokemonSeen.push(selection.value);
+        pokemonCaught.push(selection.value);
+    }
+    generatePokemon();
+
+});
+
+
+function generatePokemon() {
+    let loopArray = [];
+    for (let i = 0; i < selectionImages.length; i++) {
+        let newPokemon = randomEncounter(pokemonList);
+        loopArray.push(newPokemon.id);
+        console.log(loopArray);
+        const compare = findById(loopArray, newPokemon.id);
+        if (compare === false) {
+            selectionImages[i].src = newPokemon.url_image;
+            selection[i].value = newPokemon.id;
+        } else {
+            newPokemon = randomEncounter(pokemonList);
+        } 
+    }
 }
--- main-page/utils.js
+++ main-page/utils.js
@@ -1,6 +1,19 @@
+import { pokemonList } from '../pokemon-data/poke-data.js';
+
+
 //functions to export
 
 export function randomEncounter(someArray) {
     const index = Math.floor(Math.random() * someArray.length);
     return (someArray[index]);
 }
+
+
+export function findById(someArray, someId) {
+
+    for (let i = 0; i < someArray.length; i++)
+        if (someArray[i].id === someId) {
+            return true;
+        }
+}
+
